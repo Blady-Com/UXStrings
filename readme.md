e2de0b7626b66d3051ed607d4a003be9b24c0363
Blady <blady.net@orange.fr>
2020-11-11 17:02:55 +0100

    Initial commit with a first POC implementation, see readme.md.

---
+++ readme.md
@@ -0,0 +1,118 @@
+# Unicode Extended Strings (UXStrings)
+
+## Motivation
+
+Ada GUI library [Gnoga](https://sourceforge.net/projects/gnoga) internal character strings implementation is based on both Ada types String and Unbounded\_String.
+The native Ada String encoding is Latin-1 whereas transactions with the Javascript part are in UTF-8 encoding.
+
+Some drawbacks come up, for instance, with internationalization of programs (see [Localize Gnoga demo](https://sourceforge.net/p/gnoga/code/ci/dev_1.6/tree/demo/localize)):
+
+* several conversions between String and Unbounded\_String objects
+* it isn't usable out of Latin-1 character set, characters out of Latin-1 set are blanked
+* continuous conversions between Latin-1 and UTF-8, each sent and received transaction between Ada and Javascript parts
+
+Two ways of possible improvement for native Ada String: dynamic length handling and Unicode support.
+
+## Workarounds
+
+First possibility is using UTF-8 as internal implementation in Unbounded\_String objects.
+The simplest way but Gnoga uses many times character indexes to parse Javascript messages that is not easy to achieved with UTF-8 which may have several lengths to represent one character. String parsing will be time consuming. Some combinations may lead to incorrect UTF-8 representation.
+
+Second possibility is to use Unbounded\_Wide\_String or Unbounded\_Wide\_Wide\_String.
+Using Unbounded\_Wide\_String is quite being in the middle of the river might as well use Unbounded\_Wide\_Wide\_String. In this latter case the memory penalty is heavy for only few accentuated character occurrences. So back to Unbounded\_Wide\_String but you'll miss the so essential emojis ;-)
+
+Third possibility is to make no choice between Latin-1, Wide and Wide\_Wide characters. The object shall adapt its inner implementation to the actual content.
+For instance with English language the most often use case will be Latin-1 inner implementation, for French language the most often will be Latin-1 with some exceptions with BMP (Unicode Basic Multilingual Plane) implementation such as in "cœur", for Greek language the most often will be BMP implementation.
+The programmer won't make any representation choice when for example receiving UTF-8 messages:
+
+``` ada
+   S2 : UXString;
+   ...
+   S2 := "Received: " & From_UTF8 (Message);
+```
+
+Automatically S2 will adapt its inner representation to the received characters.
+
+## UXStrings packages
+
+Package named [UXStrings](https://github.com/Blady-Com/UXStrings/blob/master/src/uxstrings1.ads) (Unicode Extended String) and its [Text_IO](https://github.com/Blady-Com/UXStrings/blob/master/src/uxstrings-text_io1.ads) child package are proposed to bring String enhancements using some Ada 202x features.
+
+The first part of UXString package contains renaming statements of current Ada types.
+Ada current String type is structurally an array of Latin-1 characters thus is renamed as Latin\_1\_Character\_Array.
+And so on.
+
+The second part defines the USXString type as a tagged private type which has got aspects such as Constant\_Indexing, Variable\_Indexing, Iterable and String_Literal, so we can write:
+
+``` ada
+   S1, S2, S3 : UXString;
+   C          : Character;
+   WC         : Wide_Character;
+   WWC        : Wide_Wide_Character;
+   ...
+   S1 := "était blah blah";
+   C   := S1 (3);
+   WC  := S1 (2);
+   WWC := S1 (1);
+   S1 (3) := WWC;
+   S1 (2) := WC;
+   S1 (1) := C;
+   S3  := "une soirée passée à étudier les mathématiques ℕ⊂𝕂...";
+   for I in S3 loop
+      C   := S3 (I);
+      WC  := S3 (I);
+      WWC := S3 (I);
+      Put_Line (Character'pos (C)'img & Wide_Character'pos (WC)'img & Wide_Wide_Character'pos (WWC)'img);
+   end loop;
+```
+
+The third part defines conversion functions between UXString and various encoding such as Latin-1, BMP (USC-2), Unicode (USC-4), UTF-8 or UTF-16, so we can write:
+
+``` ada
+   S1  := From_Latin_1 ("blah blah");
+   S2  := From_BMP ("une soirée passée à étudier la physique ω=Δθ/Δt...");
+   S3  := From_Unicode ("une soirée passée à étudier les mathématiques ℕ⊂𝕂...");
+   Send (To_UTF_8 (S1) & To_UTF_8 (S3));
+```
+
+The fourth part defines various API coming from Unbounded\_String such as Append, "&", Slice, "=", Index and so on.
+
+Note: Iterable is a GNAT specific aspect.
+
+## UXStrings implementations
+
+### UXStrings 1
+
+A first POC implementation is provided. The source code files are ending with the number 1 as for instance "uxstrings1.ads". A GNAT project file "uxstrings1.gpr" is provided with some naming conventions for both packages UXStrings  and UXStrings.Text\_IO.
+
+#### Implementation choices
+
+UTF-8 encoding is chosen for internal representation. The [Strings_Edit library](http://www.dmitry-kazakov.de/ada/strings_edit.htm) is used for UTF-8 encoding management.
+[GNAT.OS_Lib](https://docs.adacore.com/gnat_rm-docs/html/gnat_rm/gnat_rm/the_gnat_library.html#gnat-os-lib-g-os-lib-ads) is chosen for input / output management.
+
+This implementation which is only for demonstrate the possible usages of UXString has many limitations.
+
+#### Limitations:
+
+- no memory deallocation
+- not thread safe
+- single character assignment is not implemented
+- only few API are implemented
+
+### Future implementations
+
+Here are some ideas:
+
+- Use memory management as implemented in XStrings from [GNATColl](https://github.com/AdaCore/gnatcoll-core/blob/master/src/gnatcoll-strings_impl.ads).
+- Use Unbounded\_Strings with UTF-8 encoding.
+- Use Unbounded\_Wide\_Wide\_Strings
+- Adapt the inner implementation to the actual content with 8 bits character encodings, 16 bits or 32 bits.
+
+## Tests
+
+One test program [test\_uxstrings.adb](https://github.com/Blady-Com/UXStrings/blob/master/tests/test_uxstrings.adb) is provided for UXStrings tests and an other test program [test\_uxstrings\_text\_io.adb](https://github.com/Blady-Com/UXStrings/blob/master/tests/test_uxstrings_text_io.adb) is provided for UXStrings.Text\_IO tests.
+
+## Feedbacks
+
+Feel free to send feedback about UXStrings specification source code on [Github](https://github.com/Blady-Com/UXStrings/issues).
+
+Pascal Pignard, November 2020.
---
+++ src/uxstrings1.ads
@@ -0,0 +1,196 @@
+with Ada.Strings;                use Ada.Strings;
+with Ada.Strings.Wide_Wide_Maps; use Ada.Strings.Wide_Wide_Maps;
+private with Ada.Finalization;
+
+package UXStrings is
+
+   type Encoding_Scheme is (Latin_1, UTF_8, UTF_16BE, UTF_16LE);
+
+   -- ISO/IEC 8859-1
+   subtype Latin_1_Character is Character;
+   subtype Latin_1_Character_Array is String;
+
+   -- Unicode Basic Multilingual Plane
+   -- Could be also named UCS_2_Character (Universal Coded Character Set)?
+   subtype BMP_Character is Wide_Character;
+   subtype BPM_Character_Array is Wide_String;
+
+   -- Unicode planes
+   -- Could be also named UCS_4_Character?
+   subtype Unicode_Character is Wide_Wide_Character;
+   subtype Unicode_Character_Array is Wide_Wide_String;
+
+   subtype UTF_8_Character is Character;
+   type UTF_8_Character_Array is array (Positive range <>) of UTF_8_Character;
+
+   subtype UTF_16_Encoding_Scheme is Encoding_Scheme range UTF_16BE .. UTF_16LE;
+   subtype UTF_16_Character is Wide_Character;
+   type UTF_16_Character_Array is array (Positive range <>) of UTF_16_Character;
+
+   subtype Char_Type is Latin_1_Character;
+   subtype Wide_Char_Type is BMP_Character;
+   subtype Wide_Wide_Char_Type is Unicode_Character;
+
+   type UXString is tagged private with
+      Constant_Indexing => Element,
+      Variable_Indexing => Reference,
+      Iterable          => (First => First, Next => Next, Has_Element => Has_Element, Element => Element),
+      String_Literal    => From_Unicode;
+
+   Null_UXString : constant UXString;
+
+   function Length (Source : UXString) return Natural;
+
+   function Element (Source : UXString; Index : Positive; Substitute : in Char_Type := '¿') return Char_Type;
+   function Element (Source : UXString; Index : Positive; Substitute : in Wide_Char_Type := '¿') return Wide_Char_Type;
+   function Element (Source : UXString; Index : Positive) return Wide_Wide_Char_Type;
+
+   type Character_Reference (Char : not null access Char_Type) is limited private with
+      Implicit_Dereference => Char;
+   function Reference (Source : aliased in out UXString; Index : Positive) return Character_Reference;
+   type Wide_Character_Reference (Wide_Char : not null access Wide_Char_Type) is limited private with
+      Implicit_Dereference => Wide_Char;
+   function Reference (Source : aliased in out UXString; Index : Positive) return Wide_Character_Reference;
+   type Wide_Wide_Character_Reference (Wide_Wide_Char : not null access Wide_Wide_Char_Type) is limited private with
+      Implicit_Dereference => Wide_Wide_Char;
+   function Reference (Source : aliased in out UXString; Index : Positive) return Wide_Wide_Character_Reference;
+
+   function First (Source : UXString) return Positive;
+   function Next (Source : UXString; Index : Positive) return Positive;
+   function Has_Element (Source : UXString; Index : Positive) return Boolean;
+
+   function Is_Latin_1 (Source : UXString; Index : Positive) return Boolean;
+   function Is_Latin_1 (Source : UXString) return Boolean;
+   function To_Latin_1
+     (Source : UXString; Index : Positive; Substitute : in Latin_1_Character := '¿') return Latin_1_Character;
+   function To_Latin_1 (Source : UXString; Substitute : in Latin_1_Character := '¿') return Latin_1_Character_Array;
+   function From_Latin_1 (Str : Latin_1_Character_Array) return UXString;
+
+   function Is_BMP (Source : UXString; Index : Positive) return Boolean;
+   function Is_BMP (Source : UXString) return Boolean;
+   function To_BPM (Source : UXString; Index : Positive; Substitute : in BMP_Character := '¿') return BMP_Character;
+   function To_BPM (Source : UXString; Substitute : in BMP_Character := '¿') return BPM_Character_Array;
+   function From_BMP (Str : BPM_Character_Array) return UXString;
+
+   function Is_Unicode (Source : UXString; Index : Positive) return Boolean;
+   function Is_Unicode (Source : UXString) return Boolean;
+   function To_Unicode (Source : UXString; Index : Positive) return Unicode_Character;
+   function To_Unicode (Source : UXString) return Unicode_Character_Array;
+   function From_Unicode (Str : Unicode_Character_Array) return UXString;
+
+   function To_UTF_8 (Source : UXString; Output_BOM : Boolean := False) return UTF_8_Character_Array;
+   function From_UTF8 (Str : UTF_8_Character_Array) return UXString;
+
+   function To_UTF_16
+     (Source : UXString; Output_Scheme : UTF_16_Encoding_Scheme; Output_BOM : Boolean := False)
+      return UTF_16_Character_Array;
+   function From_UTF_16 (Str : UTF_16_Character_Array; Input_Scheme : UTF_16_Encoding_Scheme) return UXString;
+
+   procedure Set (Target : out UXString; Unicode_Source : Unicode_Character_Array);
+
+   procedure Append (Source : in out UXString; New_Item : UXString);
+   procedure Append (Source : in out UXString; New_Wide_Wide_Item : Unicode_Character);
+
+   function "&" (Left : UXString; Right : UXString) return UXString;
+   function "&" (Left : UXString; Right : Unicode_Character) return UXString;
+   function "&" (Left : Unicode_Character; Right : UXString) return UXString;
+
+   procedure Replace_Element (Source : in out UXString; Index : Positive; By : Unicode_Character);
+
+   function Slice (Source : UXString; Low : Positive; High : Natural) return UXString;
+   procedure Slice (Source : UXString; Target : out UXString; Low : Positive; High : Natural);
+
+   function "=" (Left : UXString; Right : UXString) return Boolean;
+   function "<" (Left : UXString; Right : UXString) return Boolean;
+   function "<=" (Left : UXString; Right : UXString) return Boolean;
+   function ">" (Left : UXString; Right : UXString) return Boolean;
+   function ">=" (Left : UXString; Right : UXString) return Boolean;
+
+   ------------------------
+   -- Search Subprograms --
+   ------------------------
+
+   function Index
+     (Source  : UXString; Pattern : UXString; Going : Direction := Forward;
+      Mapping : Wide_Wide_Character_Mapping := Identity) return Natural;
+   function Index
+     (Source  : UXString; Pattern : UXString; Going : Direction := Forward;
+      Mapping : Wide_Wide_Character_Mapping_Function) return Natural;
+   function Index
+     (Source : UXString; Set : Wide_Wide_Character_Set; Test : Membership := Inside; Going : Direction := Forward)
+      return Natural;
+   function Index
+     (Source  : UXString; Pattern : UXString; From : Positive; Going : Direction := Forward;
+      Mapping : Wide_Wide_Character_Mapping := Identity) return Natural;
+   function Index
+     (Source  : UXString; Pattern : UXString; From : Positive; Going : Direction := Forward;
+      Mapping : Wide_Wide_Character_Mapping_Function) return Natural;
+   function Index
+     (Source : UXString; Set : Wide_Wide_Character_Set; From : Positive; Test : Membership := Inside;
+      Going  : Direction := Forward) return Natural;
+
+   function Index_Non_Blank (Source : UXString; Going : Direction := Forward) return Natural;
+   function Index_Non_Blank (Source : UXString; From : Positive; Going : Direction := Forward) return Natural;
+
+   function Count
+     (Source : UXString; Pattern : UXString; Mapping : Wide_Wide_Character_Mapping := Identity) return Natural;
+   function Count
+     (Source : UXString; Pattern : UXString; Mapping : Wide_Wide_Character_Mapping_Function) return Natural;
+   function Count (Source : UXString; Set : Wide_Wide_Character_Set) return Natural;
+
+   procedure Find_Token
+     (Source :     UXString; Set : Wide_Wide_Character_Set; From : Positive; Test : Membership; First : out Positive;
+      Last   : out Natural);
+   procedure Find_Token
+     (Source : UXString; Set : Wide_Wide_Character_Set; Test : Membership; First : out Positive; Last : out Natural);
+
+   ------------------------------------
+   -- String Translation Subprograms --
+   ------------------------------------
+
+   function Translate (Source : UXString; Mapping : Wide_Wide_Character_Mapping) return UXString;
+   procedure Translate (Source : in out UXString; Mapping : Wide_Wide_Character_Mapping);
+   function Translate (Source : UXString; Mapping : Wide_Wide_Character_Mapping_Function) return UXString;
+   procedure Translate (Source : in out UXString; Mapping : Wide_Wide_Character_Mapping_Function);
+
+   ---------------------------------------
+   -- String Transformation Subprograms --
+   ---------------------------------------
+
+   function Replace_Slice (Source : UXString; Low : Positive; High : Natural; By : UXString) return UXString;
+   procedure Replace_Slice (Source : in out UXString; Low : Positive; High : Natural; By : UXString);
+   function Insert (Source : UXString; Before : Positive; New_Item : UXString) return UXString;
+   procedure Insert (Source : in out UXString; Before : Positive; New_Item : UXString);
+   function Overwrite (Source : UXString; Position : Positive; New_Item : UXString) return UXString;
+   procedure Overwrite (Source : in out UXString; Position : Positive; New_Item : UXString);
+   function Delete (Source : UXString; From : Positive; Through : Natural) return UXString;
+   procedure Delete (Source : in out UXString; From : Positive; Through : Natural);
+   function Trim (Source : UXString; Side : Trim_End) return UXString;
+   procedure Trim (Source : in out UXString; Side : Trim_End);
+   function Trim (Source : UXString; Left : Wide_Wide_Character_Set; Right : Wide_Wide_Character_Set) return UXString;
+   procedure Trim (Source : in out UXString; Left : Wide_Wide_Character_Set; Right : Wide_Wide_Character_Set);
+   function Head (Source : UXString; Count : Natural; Pad : Unicode_Character := Wide_Wide_Space) return UXString;
+   procedure Head (Source : in out UXString; Count : Natural; Pad : Unicode_Character := Wide_Wide_Space);
+   function Tail (Source : UXString; Count : Natural; Pad : Unicode_Character := Wide_Wide_Space) return UXString;
+   procedure Tail (Source : in out UXString; Count : Natural; Pad : Unicode_Character := Wide_Wide_Space);
+
+   function "*" (Left : Natural; Right : UXString) return UXString;
+   function "*" (Left : Natural; Right : Unicode_Character) return UXString;
+
+private
+   type Character_Reference (Char : not null access Char_Type) is null record;
+   type Wide_Character_Reference (Wide_Char : not null access Wide_Char_Type) is null record;
+   type Wide_Wide_Character_Reference (Wide_Wide_Char : not null access Wide_Wide_Char_Type) is null record;
+
+   type UTF_8_Characters_Access is access UTF_8_Character_Array;
+   type UXString is new Ada.Finalization.Controlled with record
+      Chars          : UTF_8_Characters_Access;
+      Char           : aliased Char_Type;
+      Wide_Char      : aliased Wide_Char_Type;
+      Wide_Wide_Char : aliased Wide_Wide_Char_Type;
+      Index          : Natural := 0;
+   end record;
+
+   Null_UXString : constant UXString :=
+     (Ada.Finalization.Controlled with Chars => new UTF_8_Character_Array (2 .. 1), others => <>);
+end UXStrings;
